2730651e8035b79837320f3a832e0cddb797c036
SOVEREIGN <isimemeni@gmail.com>
2020-12-01 18:56:25 -0500

    Added css stylings

--- natours/src/App.js
+++ natours/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import logo from './img/logo-white.png'
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <div className="logo-box">
+          <img src={logo} alt="Logo" className="logo"></img>
+        </div>
+        <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub"> is where life happens</span>
+          </h1>
+          </div>
+      </header>
     </div>
   );
 }
