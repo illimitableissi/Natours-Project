5931f182b7d9db9fd9f7c5a7e8a92ac1a7fa7e68
SOVEREIGN <isimemeni@gmail.com>
2020-12-02 22:48:16 -0500

    added BEM className formatting

--- natours/src/Components/Header.js
+++ natours/src/Components/Header.js
@@ -5,15 +5,15 @@ function Header() {
   return (
     <div className="App">
       <header className="header">
-        <div className="logo-box">
-          <img src={logo} alt="Logo" className="logo"></img>
+        <div className="header__logo-box">
+          <img src={logo} alt="Logo" className="header__logo"></img>
         </div>
-        <div className="text-box">
+        <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub"> is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub"> is where life happens</span>
           </h1>
-          <a href="#" className="btn btn-white btn-animated">Discover our tours</a>
+          <a href="#" className="btn btn--white btn--animated">Discover our tours</a>
           </div>
       </header>
     </div>
