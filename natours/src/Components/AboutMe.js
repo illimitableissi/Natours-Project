aad1a2808a544793f0d9f6078d8529ad90452da7
SOVEREIGN <isimemeni@gmail.com>
2020-12-07 22:02:19 -0500

    working on About Me section

--- natours/src/Components/AboutMe.js
+++ natours/src/Components/AboutMe.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import '../sass/main.sass';
-// import Row from 'react-bootstrap';
-// import Col from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const AboutMe = () => {
     return (
         <div className="section-about"> 
-            <div className="u-center-text">
-            <h2 className="heading-secondary">
-                Exciting tours for adventurous people.
-            </h2>
+            <div className="u-center-text u-margin-bottom-big">
+                <h2 className="heading-secondary">
+                    Exciting tours for adventurous people.
+                </h2>
             </div>
+            <Row>
+                <Col>
+                    <h3 className="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
+                    <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque vel cupiditate quae obcaecati consequatur distinctio! Voluptas magnam incidunt eveniet laboriosam, porro dignissimos veritatis voluptatibus numquam harum sint accusantium, quasi est!</p>
+                    <h3 className="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
+                    <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque vel cupiditate quae obcaecati consequatur distinctio! Voluptas magnam incidunt eveniet laboriosam</p>
+                    <a href="#" className="btn-text">Learn more -{">"}</a>
+                </Col>
+                <Col>
+                    Image composition
+                </Col>
+            </Row>
         </div>
     );
 };
