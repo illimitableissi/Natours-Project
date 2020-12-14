79de68bf333546bc22970fcb69c9692c033eebdb
SOVEREIGN <isimemeni@gmail.com>
2020-12-13 21:00:44 -0500

    working on Features section

--- natours/src/App.js
+++ natours/src/App.js
@@ -1,11 +1,13 @@
 import Header from './Components/Header'
 import AboutMe from './Components/AboutMe'
+import Features from './Components/Features'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AboutMe />
+      <Features />
     </div>
   );
 }
---
+++ natours/src/Components/Features.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../iconStyles.css'
+
+const Features = () => {
+    return (
+        <div className="section-features"> 
+            <i className="icon-basic-world"></i>
+        </div>
+    );
+};
+
+export default Features;
