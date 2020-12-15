a37b25b50f4007a017f0956b48f6c893cb922f15
SOVEREIGN <isimemeni@gmail.com>
2020-12-14 22:00:46 -0500

    added components

--- natours/src/App.js
+++ natours/src/App.js
@@ -1,6 +1,7 @@
 import Header from './Components/Header'
 import AboutMe from './Components/AboutMe'
 import Features from './Components/Features'
+import Tours from './Components/Tours'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <AboutMe />
       <Features />
+      <Tours />
     </div>
   );
 }
