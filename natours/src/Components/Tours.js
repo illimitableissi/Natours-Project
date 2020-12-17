6968e9e3bc0203b77d2557ac6147eed6532c09ec
SOVEREIGN <isimemeni@gmail.com>
2020-12-16 22:44:51 -0500

    working on button and card issues

--- natours/src/Components/Tours.js
+++ natours/src/Components/Tours.js
@@ -19,7 +19,7 @@ const Tours = () => {
                             <div className="card__picture card__picture--1">
                             </div>
                             <h4 className="card__heading">
-                                <span className="card__heading-span card__heading-span--1"> The Sea Explorer</span>
+                                <span className="card__heading-span card__heading-span--1">The Sea Explorer</span>
                             </h4>
                             <div className="card__details">
                                 <ul>
@@ -31,16 +31,79 @@ const Tours = () => {
                                 </ul>
                             </div>     
                             </div>
-                        <div className="card__side card__side--back card__side--back-1">  
-                            Back    
+                        <div className="card__side card__side--back card__side--back-1">
+                            <div className="card__cta">
+                                <div className="card__price-box">
+                                    <p className="card__price-only">Only</p>
+                                    <p className="card__price-value">$297</p>
+                                </div>
+                                <a href="#" className="btn btn--white">Book now!</a>
+                            </div>   
                         </div>
                     </div>
                 </Col>
                 <Col>
+                <div className="card">
+                        <div className="card__side card__side--front">
+                            <div className="card__picture card__picture--2">
+                            </div>
+                            <h4 className="card__heading">
+                                <span className="card__heading-span card__heading-span--2">The Forest Hiker</span>
+                            </h4>
+                            <div className="card__details">
+                                <ul>
+                                    <li>7 day tour</li>
+                                    <li>Up to 40 people</li>
+                                    <li>6 tour guides</li>
+                                    <li>Sleep in provided tents</li>
+                                    <li>Difficulty: medium</li>
+                                </ul>
+                            </div>     
+                            </div>
+                        <div className="card__side card__side--back card__side--back-2">
+                            <div className="card__cta">
+                                <div className="card__price-box">
+                                    <p className="card__price-only">Only</p>
+                                    <p className="card__price-value">$497</p>
+                                </div>
+                                <a href="#" className="btn btn--white">Book now!</a>
+                            </div>   
+                        </div>
+                    </div>
                 </Col>
                 <Col>
+                <div className="card">
+                        <div className="card__side card__side--front">
+                            <div className="card__picture card__picture--3">
+                            </div>
+                            <h4 className="card__heading">
+                                <span className="card__heading-span card__heading-span--3">The Snow Adventurer</span>
+                            </h4>
+                            <div className="card__details">
+                                <ul>
+                                    <li>5 day tour</li>
+                                    <li>Up to 15 people</li>
+                                    <li>3 tour guides</li>
+                                    <li>Sleep in provided tents</li>
+                                    <li>Difficulty: hard</li>
+                                </ul>
+                            </div>     
+                            </div>
+                        <div className="card__side card__side--back card__side--back-3">
+                            <div className="card__cta">
+                                <div className="card__price-box">
+                                    <p className="card__price-only">Only</p>
+                                    <p className="card__price-value">$897</p>
+                                </div>
+                                <a href="#" className="btn btn--white">Book now!</a>
+                            </div>   
+                        </div>
+                    </div>
                 </Col>
             </Row>
+            <div className="u-center-text u-margin-top-huge">
+                <a href="#" className="btn btn--green">Discover all tours</a>
+            </div>
             </Container>
         </div>
     );
