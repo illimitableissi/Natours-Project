a65c3cf959719d86569a56f6975ca8c9450b320b
SOVEREIGN <isimemeni@gmail.com>
2020-12-16 21:14:52 -0500

    working on card size issues

--- natours/src/Components/Tours.js
+++ natours/src/Components/Tours.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import Row from 'react/bootstrap/Row'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 const Tours = () => {
     return (
         <div className="section-tours">
+        <Container>
             <div className="u-center-text u-margin-bottom-big">
-            <h2 className="heading-secondary">
-                Exciting tours for adventurous people.
-            </h2>
+                <h2 className="heading-secondary">
+                    Most popular tours
+                </h2>
             </div>
+            <Row>
+                <Col>
+                    <div className="card">
+                        <div className="card__side card__side--front">
+                            Front    
+                        </div>
+                        <div className="card__side card__side--back card__side--back-1">  
+                            Back    
+                        </div>
+                    </div>
+                </Col>
+                <Col>
+                </Col>
+                <Col>
+                </Col>
+            </Row>
+            </Container>
         </div>
     );
 };
