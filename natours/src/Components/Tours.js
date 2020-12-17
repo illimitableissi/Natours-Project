f83694421793f06bbdd12d0e7cdd19654bee75e8
SOVEREIGN <isimemeni@gmail.com>
2020-12-16 20:21:28 -0500

    working on tours section

--- natours/src/Components/Tours.js
+++ natours/src/Components/Tours.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Row from 'react/bootstrap/Row'
 
 const Tours = () => {
     return (
-        <div>
-            
+        <div className="section-tours">
+            <div className="u-center-text u-margin-bottom-big">
+            <h2 className="heading-secondary">
+                Exciting tours for adventurous people.
+            </h2>
+            </div>
         </div>
     );
 };
