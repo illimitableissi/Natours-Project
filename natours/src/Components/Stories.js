e2c298767368b07565d6b4b9285bf4ef28fea620
SOVEREIGN <isimemeni@gmail.com>
2020-12-18 19:40:17 -0500

    stuff

--- natours/src/Components/Stories.js
+++ natours/src/Components/Stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Stories = () => {
     return (
         <div className="section-stories">
-            
+            <div className="section stuff"></div>
         </div>
     );
 };
