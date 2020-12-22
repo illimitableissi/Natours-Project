a46fb51f6126ed5bde3020165f4c19d98db5682d
SOVEREIGN <isimemeni@gmail.com>
2020-12-21 22:53:32 -0500

    working on booking section

--- natours/src/App.js
+++ natours/src/App.js
@@ -15,6 +15,7 @@ function App() {
       <Features />
       <Tours />
       <Stories />
+      <Booking />
     </div>
   );
 }
--- natours/src/Components/Booking.js
+++ natours/src/Components/Booking.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
 
 const Booking = () => {
     return (
-        <div className="section-booking">
-            
+        <div className="section-book">
+            <Container>
+                <Row>
+                    <div className="book">
+                        <div className="book__form">
+                            <form action="#" className="form">
+                                <div className="form_group">
+                                    <input type="text" className="form__input" placeholder="Full Name" in="name" required></input>
+                                    <label for="name" className="form__label">Full name</label>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </Row> 
+            </Container>
         </div>
     );
 };
